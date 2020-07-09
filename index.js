4f0b4cabb399782029ac31b5fee89299a1191ff4
wangxk <wangxk@fun.tv>
2020-07-09 10:46:58 +0800

    {feat}init

---
+++ index.js
@@ -0,0 +1,12 @@
+(function(){
+	let {FileUpload } = require('./lib/index.js');
+	//兼容CommonJs规范
+	if (typeof module !== 'undefined' && module.exports) {
+    	module.exports = FileUpload;
+    }
+    //兼容AMD/CMD规范
+    if (typeof define === 'function'){
+    	define(function() { return FileUpload; });
+    }
+    self.FileUpload = FileUpload;
+})( self )
---
+++ webpack.config.js
@@ -0,0 +1,118 @@
+const polyfill = []
+const os = require('os');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const resolve = dir => path.resolve(__dirname, dir);
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV == 'development'
+const WEB_ENV = process.env.WEB_ENV;
+
+let productionDir = ``;
+
+const config = {
+  entry: "./index.js",
+  output: {
+    path: isDev ? `${__dirname}/dist`:productionDir,
+    filename: 'index.js',
+  },
+  resolve: {
+    // 设置别名
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': resolve('src')// 这样配置后 @ 可以指向 src 目录
+    }
+  },
+  module: {
+    rules: [
+      { 
+        test:/\.styl(us)?$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+            //选项的作用使用来提高效率的。
+          },
+          'stylus-loader'
+        ]
+      },
+      { 
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+            //选项的作用使用来提高效率的。
+          },
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(jsx)$/, exclude: /node_modules/, loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }, 
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              minimize: false
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test:/\.less$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|psd|svg|icon)$/,
+　　　　loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+      }
+    ]
+  },
+  mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      'dev':JSON.stringify(process.env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin( { filename: "index.html", template: path.join(__dirname, "./index.html") } ),
+    
+    //new MiniCssExtractPlugin({filename: 'style.css',})
+    new webpack.HotModuleReplacementPlugin()
+    
+  ],
+  devServer: {   
+    //contentBase: false, // boolean | string | array, static file location
+    contentBase:"./dist",
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+    // ...
+  },
+  optimization: {
+    minimize: false
+  }
+}
+
+module.exports = config;
